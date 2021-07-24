c31bb55e55f434488acf187e0a5565e1156cfb77
SangWon7242 <gotkdqja@gmail.com>
2021-07-24 17:00:47 +0900

    UsrArticleController가 가지고 있던 서비스 로직들을 ArticleService로 옮김

--- src/main/java/com/psw/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/psw/exam/demo/controller/UsrHomeController.java
@@ -5,24 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class UsrHomeController {	
-	private int count;
-
-	public UsrHomeController() {
-		count = -1;
-	}
-	
-	@RequestMapping("/usr/home/getCount")
+public class UsrHomeController {		
+	@RequestMapping("/usr/home/main")
 	@ResponseBody
-	public int getCount() {
-		return count;
+	public String getString() {
+		return "안녕하세요";
 	}
-	
-	@RequestMapping("/usr/home/doSetCount")
-	@ResponseBody
-	public String doSetCount(int count) {
-		this.count = count;
-		return "count 값이 " + this.count + "으로 초기화 되었습니다.";
-	}
-
 }
